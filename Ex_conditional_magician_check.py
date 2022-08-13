1d2e52bb38b914518c72ee99742a5638a0f34f63
KFHK <ssk_8003@hotmail.com>
2022-08-13 20:13:52 +0100

    loop and conditional check exercises

---
+++ Ex_conditional_magician_check.py
@@ -0,0 +1,9 @@
+is_magician = False
+is_expert = True 
+
+if is_magician and is_expert:
+    print('You are a master magician.')
+elif is_magician and not(is_expert):
+    print('At least you are getting there')
+elif not is_magician:
+    print('You need magic powers')
