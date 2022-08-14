ce5b44271de76805d6e0d9fdc2e853fc55767c90
wongp.1984@gmail.com <ssk_8003@hotmail.com>
2022-08-14 19:40:03 +0100

    Enhancement to illustrate the \'self\' concept

--- Ex_class_static_method.py
+++ Ex_class_static_method.py
@@ -15,6 +15,9 @@ class PlayChar:
         else:
             print(f'{greeting} I am NOT member')
 
+    def ReturnSelf(self):
+        return self
+
     @classmethod
     def CreatePlayer(cls, name, age):
         return cls(name, age)  # instantiate the self class and return
@@ -31,10 +34,12 @@ PlayChar.SimpleStaticFunc('Dummy', 88)
 player3 = PlayChar.CreatePlayer('Kate', 38)
 print(player3.name)
 
+print(player1.ReturnSelf().ReturnSelf().ReturnSelf())
+
 
 # player1.run()
 # player1.name = 'Mama'
 # print(player1.membership)
 
-player1.abc = True
-player1.checkmember('Good morning')
+# player1.abc = True
+# player1.checkmember('Good morning')
