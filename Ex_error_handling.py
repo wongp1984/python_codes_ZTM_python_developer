63d5a7ecd7422e1cfbbfcdc3179c31c6696bf9ea
wongp.1984@gmail.com <ssk_8003@hotmail.com>
2022-08-15 15:09:52 +0100

    Add the error handlding ex

---
+++ Ex_error_handling.py
@@ -0,0 +1,14 @@
+while True:
+    try:
+        age = int(input(('what is your age?')))
+        10/age
+        raise ValueError('hey cut it out')
+    except (ValueError, ZeroDivisionError) as err:
+        print(f'Ops..... Error msg: {err}')
+    except:
+        print('please check your input.')
+    else:
+        break
+    finally:
+        # finally is always run (even though execute the break above)
+        print('ok iam finally done')
